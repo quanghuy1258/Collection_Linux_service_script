4efa8fe7273474b71ed162e5cefbcd0ecba9c52a
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2019-01-08 18:23:05 +0700

    Update server mail

--- web/server/wordpress/config-localhost.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('DB_NAME', 'wordpress');
-define('DB_USER', 'wordpress');
-define('DB_PASSWORD', 'yourpasswordhere');
-define('DB_HOST', 'localhost');
-define('WP_CONTENT_DIR', '/usr/share/wordpress/wp-content');
-?>
