ab6e08c7ed684feb0a8086c7e80fce9b17aad44a
Nguyen Van Quang Huy <quanghuy1258@gmail.com>
2019-01-13 11:33:47 +0700

    Add fake isp

---
+++ isp.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+EXTERNAL_IFACE="";
+INTERNAL_IFACE="";
+GW="";
+
+# Check parameters
+if [ -z "$EXTERNAL_IFACE" ] || [ -z "$INTERNAL_IFACE" ] || [ -z "$GW" ]; then
+	echo "Check parametes";
+	exit 1;
+fi;
+
+#Check using sudo or not
+if [ $(id -u) -ne 0 ]; then
+	echo "Error: Please run as root";
+	exit 1;
+fi;
+
+cd routing/;
+make routing IFACE=$EXTERNAL_IFACE;
+cd ../;
+
+echo "===== NAT: Done =====";
+read -n1 -r -p "Press any key to continue..." key;
+
+cd static_ip/;
+make static IFACE=$INTERNAL_IFACE IP=10.10.231.2 NETMASK=255.255.255.0 GATEWAY=$GW DNS="8.8.8.8 8.8.4.4"
+cd ../;
+
+echo "===== Static IP: Done =====";
+
